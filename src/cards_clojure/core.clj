26baea1c06b6b6d87a321a7dbd3067670e134e2f
Liz Favre <egfavre@gmail.com>
2016-07-16 10:02:29 -0400

    add straights and straight flushes

--- src/cards_clojure/core.clj
+++ src/cards_clojure/core.clj
@@ -23,45 +23,23 @@
     (= 1 (count suits))))
 
 (defn straight? [hand]
-  (let [ranks (sorted-set (map :rank hand))]
-    (let [[a b c d] ranks]
-      ((= d (+ c 1) (+ b 2) (+ a 3))))))
-    
-(defn straight-flush? [hand]
-  (let [suits (set (map :suit hand))]
-       [ranks (sorted-set (map :rank hand))]
-       [[a b c d] ranks]
-    (cond
-      (= 1 (count suits))
-      ((= d (+ c 1) (+ b 2) (+ a 3))))))
-
+  (let [ranks (sort (vec (map :rank hand)))]
+    (= (nth ranks 3) (+ (nth ranks 2) 1) (+ (nth ranks 1) 2) (+ (nth ranks 0) 3))))
+  
 (defn four-of-a-kind? [hand]
   (let [ranks (set (map :rank hand))]
-    (= 1 (count ranks))))
+    ((= 1 (count ranks)))))
 
-(defn three-of-a-kind? [hand]
-  (let [[a b c d] ranks]
-    (!= a b c d))
-  (let [ranks (sorted-set (map :rank hand))]    
-    (= 2 (count ranks))))
+(defn three-of-a-kind? [hand])
+ 
 
-(defn two-pair? [hand]
-  (let [[a b c d] hand]
-    (and (= a b) (= c d))))
-    
-    
+(defn two-pair? [hand])
+      
 (defn -main []
   (let [deck (create-deck)
         hands (create-hands deck)
-        flushes (filter flush? hands)
-        straights (filter straight? hands)
-        straight-flushes (filter striaght-flushes? hands)
-        fours (filter four-of-a-kind? hands)
-        threes (filter three-of-a-kind? hands)
-        two-pairs (filter two-pair? hands)]
-    (count flushes)
-    (count straights)
-    (count stright-flushes)
-    (count fours)
-    (count threes)
-    (count two-pairs)))
+        ;flushes (filter flush? hands)]
+        ;straights (filter straight? hands)]
+        straight-flushes (filter flush? (filter straight? hands))]
+    (count straight-flushes)))
+   
